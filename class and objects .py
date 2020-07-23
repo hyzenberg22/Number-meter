ca39e744cdc8a39ecd9656bffc60c1aa468753e6
Shubhajit Sarkar <43705275+shubhajit22@users.noreply.github.com>
2020-07-23 12:46:48 +0530

    bug fixed

--- class and objects .py
+++ class and objects .py
@@ -1,27 +1,25 @@
-#This is the Number_meter
-
 class It_dept:
     teachers = {'co': 'hod', 'bio': 'miq bou', 'evs': 'shafali'}
 
     def __init__(self, st_name, st_roll, st_number):
         self.name = st_name
         self.roll = int(st_roll)
-        self.number =float(st_number)
+        self.cgpa =float(st_number)
 
     def num_meter(self):
-        if self.number > 10:
+        if self.cgpa > 10:
             print('invalid input')
-        elif self.number >= 9:
+        elif self.cgpa >= 9:
             print('He/she is Topper')
-        elif self.number >= 8 and self.number < 9:
+        elif self.cgpa >= 8 and self.cgpa < 9:
             print('He/she is a normal student')
-        elif self.number >= 5 and self.number < 8:
+        elif self.cgpa >= 5 and self.cgpa < 8:
             print('Congratulation you are a Backbencher..!')
         else:
             print('you are in wrong place ')
 
     def printdetails(self):
-        return f'The name is:-{self.name}, roll is:-{self.roll},number is {self.number}'
+        return f'His/Her Name is:-{self.name}, roll is:-{self.roll}, CGPA is {self.cgpa}'
 
 
 student_name = input('Enter Student name:- ')
@@ -31,6 +29,4 @@ cgpa=float(input('Enter your CGPA:-'))
 student_name=It_dept(student_name,roll,cgpa)
 student_name.num_meter()
 print(student_name.printdetails())
-
-
-
+print(f'The teachers are :-{student_name.teachers}')
