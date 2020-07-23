73c5cba46ded99b07cb272674e79f900953acd67
Shubhajit Sarkar <43705275+shubhajit22@users.noreply.github.com>
2020-07-23 12:35:08 +0530

    New Code updated
    restructured the code and fix problems

--- class and objects .py
+++ class and objects .py
@@ -1,90 +1,36 @@
-.. code:: ipython3
+#This is the Number_meter
 
-    
-    
-    b
-    class It_student:
-        sub=['data_structure','co','bio']
-        teachers={'co':'malobika','bio':'miq_bou','data_structure':'Miq'}
-        pass
-    roy= It_student
-    sis= It_student
-    
-    roy.name='shubhajit roy'
-    roy.gender='male'
-    roy.hight= 4.5
-    
-    sis.name='sristi chodhury'
-    sis.gender='female'
-    sis.hight= 4.2
-    
-    print(sis.teachers['bio'],sis.hight)
-    print(sis.hight)
-    
-    
+class It_dept:
+    teachers = {'co': 'hod', 'bio': 'miq bou', 'evs': 'shafali'}
 
+    def __init__(self, st_name, st_roll, st_number):
+        self.name = st_name
+        self.roll = int(st_roll)
+        self.number =float(st_number)
 
-.. parsed-literal::
+    def num_meter(self):
+        if self.number > 10:
+            print('invalid input')
+        elif self.number >= 9:
+            print('He/she is Topper')
+        elif self.number >= 8 and self.number < 9:
+            print('He/she is a normal student')
+        elif self.number >= 5 and self.number < 8:
+            print('Congratulation you are a Backbencher..!')
+        else:
+            print('you are in wrong place ')
 
-    miq_bou 4.2
-    4.2
-    
-
-.. code:: ipython3
-
-    #self and the init function in the class and object
-    class It_dept:
-        
-        teachers={'co':'hod','bio':'miq bou','evs':'shafali'} # this is the class variable and only changed by class variable but can be acessed by the instance 
-        def __init__(self,st_name,st_roll,st_number): #here the st_name is a argument , init is used as the constructer
-            self.name=st_name #here the name is a instance variable 
-            self.roll=st_roll
-            self.number=st_number
-            
-        def printdetails(self):
-            return f'The name is:-{self.name}, roll is:-{self.roll},number is {self.number}'
-        
-        def num_meter(self):
-            if self.number >10:
-                print('invalid input')
-            elif self.number >=9:
-                print('He/she is Topper')
-            elif self.number >=8 and self.number <9:
-                print('He/she is a normal student')
-            elif self.number >=5 and self.number <8:
-                print('Congratulation you are a Backbencher..!')
-            else:
-                print('you are in wrong place ')
-                
-            
-    roy=It_dept('shubhajit Roy',45,9.27)
-    sis=It_dept('sristi choudhory',28,9.10)
-    
-    print(roy.roll)
-    print(roy.name)
-    print(roy.printdetails())
-    roy.num_meter()
-    sis.num_meter()
-    print(sis.__dict__['roll'])# __dict__ is a function that returns a dictionary of the objects details even you can use specific index of the dictonary
-    print(roy.teachers['evs'])  
-    It_dept.teachers['evs']='ud'# you can change a dict by this methode 
-    
-    print(It_dept.teachers)
-    print(sis.teachers['evs'])
-    
+    def printdetails(self):
+        return f'The name is:-{self.name}, roll is:-{self.roll},number is {self.number}'
 
 
-.. parsed-literal::
+student_name = input('Enter Student name:- ')
+roll=int(input('Enter your roll no:- '))
+cgpa=float(input('Enter your CGPA:-'))
 
-    45
-    shubhajit Roy
-    The name is:-shubhajit Roy, roll is:-45,number is 9.27
-    He/she is Topper
-    He/she is Topper
-    28
-    shafali
-    {'co': 'hod', 'bio': 'miq bou', 'evs': 'ud'}
-    ud
-    
+student_name=It_dept(student_name,roll,cgpa)
+student_name.num_meter()
+print(student_name.printdetails())
+
 
 
